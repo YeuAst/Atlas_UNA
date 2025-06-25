cdcebd399497ab4a2a3ff81b110759cb98406e9d
YeuAst <yeudany.astorga@gmail.com>
2025-06-24 20:07:13 -0600

    delete
    cambios

--- actualizarV1.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-Powershell -NoProfile -ExecutionPolicy Bypass -Command "& {"Start-Process -Verb RunAs powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "winget upgrade --include-unknown --all"'}"
-
-
